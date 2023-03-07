8554427f8048485b7057b7ed1af9a6d09bed39f1
Maksim <106922768+Kalininmladshiy@users.noreply.github.com>
2023-03-07 07:48:33 +0300

    Added enviroments

--- stock/settings.py
+++ stock/settings.py
@@ -10,6 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', True)
+YANDEX_APIKEY = env('API_KEY_YANDEX')
+TRANSPORT_TAX = env.decimal('TRANSPORT_TAX')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
