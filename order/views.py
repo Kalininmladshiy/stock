b3cae2e01657d94cad6cf8dc7d8bb43fbe727fdd
Maksim <106922768+Kalininmladshiy@users.noreply.github.com>
2023-03-07 08:33:51 +0300

    Added main paige

--- order/views.py
+++ order/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from order.models import Order
 
-# Create your views here.
+
+def view_stocks(request):
+    product_and_stocks = Order.objects.all()
+    return render(request, template_name='index.html', context={
+        'orders': product_and_stocks,
+    })
